6a092b9b2018fb5216abc2731b372c86f7350e5d
Albert Netymk <albertnetymk@gmail.com>
2013-03-27 15:51:32 +0100

    expandtab.

--- include/matmul.h
+++ include/matmul.h
@@ -17,40 +17,39 @@
 #define _PING   0
 #define _PONG   1
 
-
 typedef struct {
-	int    coreID;
-	int    corenum;
-	int    row;
-	int    col;
+    int    coreID;
+    int    corenum;
+    int    row;
+    int    col;
 
-	void  *bank[_Nbanks][2];   // Ping Pong Bank local space pointer
-	void  *tgt_bk[_Nbanks][2]; // Target Bank for matrix rotate in global space
+    void  *bank[_Nbanks][2];   // Ping Pong Bank local space pointer
+    void  *tgt_bk[_Nbanks][2]; // Target Bank for matrix rotate in global space
 
-	int    synch;     // Sync with horizontal peer core
-	int    syncv;     // Sync with vertical peer core
-	int   *tgt_synch; // ptr to synch of target neighbor
-	int   *tgt_syncv; // ptr to syncv of target neighbor
-	
-	int    pingpong; // Ping-Pong bank select indicator
+    int    synch;     // Sync with horizontal peer core
+    int    syncv;     // Sync with vertical peer core
+    int   *tgt_synch; // ptr to synch of target neighbor
+    int   *tgt_syncv; // ptr to syncv of target neighbor
 
-	int    count;
+    int    pingpong; // Ping-Pong bank select indicator
+
+    int    count;
 } core_t;
 
 
 typedef struct {
-	int    ready[_Ncores]; // Core is ready after reset
-	int    go[_Ncores];    // Call for matmul function
-	int    cycles[_Ncores];
+    int    ready[_Ncores]; // Core is ready after reset
+    int    go[_Ncores];    // Call for matmul function
+    int    cycles[_Ncores];
 } mbox_t;
 
 
 typedef struct {
-	float  A[_Smtx * _Smtx]; // Global A matrix 
-	float  B[_Smtx * _Smtx]; // Global B matrix 
-	float  C[_Smtx * _Smtx]; // Global C matrix
-	int dummy[_Ncores];
-	mbox_t core;
+    float  A[_Smtx * _Smtx]; // Global A matrix 
+    float  B[_Smtx * _Smtx]; // Global B matrix 
+    float  C[_Smtx * _Smtx]; // Global C matrix
+    int sink[10];
+    mbox_t core;
 } shared_buf_t;
 
 int timer_count;
