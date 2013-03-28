09f75ba53609a38c9e5cf421cf3f2369bd8c3cc2
Albert Netymk <albertnetymk@gmail.com>
2013-03-28 13:58:59 +0100

    Hide actors from host side.

--- include/common_buffers.h
+++ include/common_buffers.h
@@ -1,14 +1,14 @@
 #ifndef __COMMON_BUFFERS_H__
 #define __COMMON_BUFFERS_H__
 #include "matmul.h"
-#include "actors.h"
 #ifdef __HOST__
 #define SECTION(x)
 #else // __HOST__
+#include "actors.h"
 #include <e_common.h>
+volatile actors all SECTION(".shared_dram");
 #endif // __HOST__
 
 volatile shared_buf_t Mailbox SECTION(".shared_dram");
-volatile actors all SECTION(".shared_dram");
 
 #endif // __COMMON_BUFFERS_H__
