63f41bbbadd51999a0723d39b53e83ca66a4af0e
Albert Netymk <albertnetymk@gmail.com>
2013-04-12 12:56:02 +0200

    Convert to global address manually.

--- board/core0/main.c
+++ board/core0/main.c
@@ -1,9 +1,12 @@
+#include <e_coreid.h>
 #include "common.h"
+
 
 actor_a instance_a;
 volatile port_out out;
 int main(void) {
-    instance_a.out = &out;
-    core0_main(&instance_a);
+    e_coreid_t mycoreid = e_get_coreid();
+    instance_a.out = e_address_from_coreid(mycoreid, (void *)&out);
+    core0_main(e_address_from_coreid(mycoreid, (void *)&instance_a));
     return 0;
 }
--- board/core1/main.c
+++ board/core1/main.c
@@ -1,9 +1,11 @@
+#include <e_coreid.h>
 #include "common.h"
 
 actor_b instance_b;
 volatile port_in in;
 int main(void) {
-    instance_b.in = &in;
-    core1_main(&instance_b);
+    e_coreid_t mycoreid = e_get_coreid();
+    instance_b.in = e_address_from_coreid(mycoreid, (void *)&in);
+    core1_main(e_address_from_coreid(mycoreid, (void *)&instance_b));
     return 0;
 }
