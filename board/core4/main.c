b4b7a9867715749781f47e1b13882521ff10b9c5
Albert Netymk <albertnetymk@gmail.com>
2013-04-21 19:33:23 +0200

    Fix core4.

--- board/core4/main.c
+++ board/core4/main.c
@@ -19,10 +19,12 @@ int main(void) {
 actor_add instance_add;
 port_in in1;
 port_in in2;
-fifo buffer;
+fifo buffer1;
+fifo buffer2;
 int main(void) {
     e_coreid_t mycoreid = e_get_coreid();
-    in.buffer = address_from_coreid(mycoreid, &buffer);
+    in1.buffer = address_from_coreid(mycoreid, &buffer1);
+    in2.buffer = address_from_coreid(mycoreid, &buffer2);
     // local
     instance_add.in1 = address_from_coreid(mycoreid, &in1);
     instance_add.in2 = address_from_coreid(mycoreid, &in2);
@@ -35,11 +37,14 @@ int main(void) {
 actor_add instance_add;
 port_in in1;
 port_in in2;
-fifo b0, b1;
+fifo in1_b0, in1_b1;
+fifo in2_b0, in2_b1;
 int main(void) {
     e_coreid_t mycoreid = e_get_coreid();
-    in.buffers[0] = address_from_coreid(mycoreid, &b0);
-    in.buffers[1] = address_from_coreid(mycoreid, &b1);
+    in1.buffers[0] = address_from_coreid(mycoreid, &in1_b0);
+    in1.buffers[1] = address_from_coreid(mycoreid, &in1_b1);
+    in2.buffers[0] = address_from_coreid(mycoreid, &in2_b0);
+    in2.buffers[1] = address_from_coreid(mycoreid, &in2_b1);
     // local
     instance_add.in1 = address_from_coreid(mycoreid, &in1);
     instance_add.in2 = address_from_coreid(mycoreid, &in2);
--- include/flags.h
+++ include/flags.h
@@ -1,8 +1,8 @@
 #ifndef FLAGS_H_8IQG2XQD
 #define FLAGS_H_8IQG2XQD
 
-#define USE_DESTINATION_BUFFER
+// #define USE_DESTINATION_BUFFER
 // #define USE_BOTH_BUFFER
-// #define USE_DOUBLE_BUFFER
+#define USE_DOUBLE_BUFFER
 
 #endif /* end of include guard: FLAGS_H_8IQG2XQD */
