0c4a0aaa5dfb4bed5f0c795307663cfb6df252f9
Albert Netymk <albertnetymk@gmail.com>
2013-06-05 23:41:32 +0200

    Remove old actors.

--- include/actors/actors.h
+++ include/actors/actors.h
@@ -3,17 +3,19 @@
 #ifndef _ACTORS_H
 #define _ACTORS_H
 
-#include "actors/actor_Scale.h"
+// #include "actors/actor_Scale.h"
 #include "actors/actor_Combine.h"
 #include "actors/actor_ShuffleFly.h"
 #include "actors/actor_Shuffle.h"
-#include "actors/actor_Final.h"
+// #include "actors/actor_Final.h"
+#include "actors/actor_Scale_source.h"
+#include "actors/actor_Final_sink.h"
 
 typedef struct actors_struct {
-    actor_Scale *instance_Scale;
+    actor_Scale_source *instance_Scale;
     actor_Combine *instance_Combine;
     actor_ShuffleFly *instance_ShuffleFly;
     actor_Shuffle *instance_Shuffle;
-    actor_Final *instance_Final;
+    actor_Final_sink *instance_Final;
 } actors;
 #endif
